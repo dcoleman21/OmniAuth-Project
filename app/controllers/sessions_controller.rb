c6be4573ef421fe09cc6d13fefa1fd1ee865b86b
Dani Coleman <danicoleman00@gmail.com>
2020-12-30 16:15:14 -0600

    Add create action calling the session facade. Add helper method for auth_hash utilizing omniauth abstraction

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,36 +1,15 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
+
   def create
-    client_id = '8fddb31ec45c0f3551c8'
-    client_secret = 'c95c3d3e3f4307580eef60c9d3e7be6cae391c6a'
-    code = params[:code]
-
-    conn = Faraday.new(url: 'https://github.com', headers: {'Accept': 'application/json'})
-
-    response = conn.post('/login/oauth/access_token') do |req|
-      req.params['code'] = code
-      req.params['client_id'] = client_id
-      req.params['client_secret'] = client_secret
-    end
-
-    data = JSON.parse(response.body, symbolize_names: true)
-    access_token = data[:access_token]
-
-    conn = Faraday.new(
-      url: 'https://api.github.com',
-      headers: {
-          'Authorization': "token #{access_token}"
-      }
-    )
-    response = conn.get('/user')
-    data = JSON.parse(response.body, symbolize_names: true)
-
-    user          = User.find_or_create_by(uid: data[:id])
-    user.username = data[:login]
-    user.uid      = data[:id]
-    user.token    = access_token
-    user.save
-
+    user = SessionFacade.session_details(auth_hash)
     session[:user_id] = user.id
     redirect_to dashboard_path
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
