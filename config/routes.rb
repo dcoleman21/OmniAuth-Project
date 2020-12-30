54d6d4e0f9a4b6eb7a41468087ba826bb461c167
Dani Coleman <danicoleman00@gmail.com>
2020-12-30 16:13:34 -0600

    Add auth/github as github_login route. Add auth/provider/callback to seesions#create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
 
-  get '/auth/github/callback', to: 'sessions#create'
+  get 'auth/github', as: 'github_login'
+  get '/auth/:provider/callback', to: 'sessions#create'
   get '/dashboard', to: 'dashboard#show'
 end
