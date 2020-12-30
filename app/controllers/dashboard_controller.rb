613f7e11ab6d001e88279357972b38f2085bd225
Dani Coleman <danicoleman00@gmail.com>
2020-12-30 16:19:15 -0600

    Make a faraday call to the github endpoint /user/repos to return a users github data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,18 @@
 class DashboardController < ApplicationController
-  def show;end
+  def show
+    conn = Faraday.new(
+      url: 'https://api.github.com',
+      headers: {
+          'Authorization': "token #{current_user.token}"
+      }
+    )
+    response = conn.get('/user/repos')
+     @repo_data = JSON.parse(response.body, symbolize_names: true)
+
+     repo_status = []
+     @repo_data.each do |data|
+       repo_status << data[:private]
+       require "pry"; binding.pry
+     end
+  end
 end
