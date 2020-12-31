1b2819b17425c6dcb11e4a50949688c41535c6d3
Dani Coleman <danicoleman00@gmail.com>
2020-12-30 18:05:43 -0600

    Add Faraday call to return json payload of github repo data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,16 +7,6 @@ class DashboardController < ApplicationController
       }
     )
     response = conn.get('/user/repos')
-     @repo_data = JSON.parse(response.body, symbolize_names: true)
-
-
-    repo_status_public = []
-    @repo_data.each do |data|
-     repo_status_public << if data[:private] == false
-     else
-      data[:private]
-      require "pry"; binding.pry
-     end
-    end
+    @repo_data = JSON.parse(response.body, symbolize_names: true)
   end
 end
